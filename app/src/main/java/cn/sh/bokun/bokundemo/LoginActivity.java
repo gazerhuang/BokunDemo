6c099b0323c30fd09cb62c8f497f3e92de076501
gazerhuang <gazerhuang@gmail.com>
2018-07-02 16:50:31 +0800

    login & qr pic

--- app/src/main/java/cn/sh/bokun/bokundemo/LoginActivity.java
+++ app/src/main/java/cn/sh/bokun/bokundemo/LoginActivity.java
@@ -2,8 +2,13 @@ package cn.sh.bokun.bokundemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView acc;
+    private EditText pwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
