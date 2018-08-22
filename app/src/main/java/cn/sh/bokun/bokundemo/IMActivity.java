d577ead8862638c74897a2aa6cf74dc01ae8ab0a
gazerhuang <gazerhuang@gmail.com>
2018-08-22 16:15:00 +0800

    IM

---
+++ app/src/main/java/cn/sh/bokun/bokundemo/IMActivity.java
@@ -0,0 +1,13 @@
+package cn.sh.bokun.bokundemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_im);
+    }
+}
